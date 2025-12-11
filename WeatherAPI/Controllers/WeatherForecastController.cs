a81cce4ef60023291c35ab791a88917cbde5e593
jdeath777 <jithin.abraham3@gmail.com>
2025-12-11 11:41:04 +1100

    Fix Git tracking — add full WeatherAPI project

---
+++ WeatherAPI/Controllers/WeatherForecastController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+
+namespace WeatherAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class WeatherForecastController : ControllerBase
+{
+   
+
+    private readonly ILogger<WeatherForecastController> _logger;
+    private readonly IHttpClientFactory _httpClientFactory;
+   
+    //using HttpClientFactory
+    public WeatherForecastController(ILogger<WeatherForecastController> logger, IHttpClientFactory httpClientFactory)
+    {
+        _logger = logger;
+        _httpClientFactory = httpClientFactory;
+      
+    }
+
+    [HttpGet(Name = "GetWeatherForecast")]
+    public async Task<string> Get(string cityName)
+    {
+        var httpClient = _httpClientFactory.CreateClient();
+        var URL = $"http://api.weatherapi.com/v1/current.json?key=4eb744e0d9bb4a27bbc75712250912&q={cityName}";
+        var response = await httpClient.GetAsync(URL);
+        return  await response.Content.ReadAsStringAsync();
+        
+
+       
+    }
+}
+
